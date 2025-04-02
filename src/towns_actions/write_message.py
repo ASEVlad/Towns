8f0df951fb0dba81c89c5444289f139d96610259
ASEVlad <kvochkinvlad@gmail.com>
2025-04-02 13:13:41 +0200

    updated write_message

--- src/towns_actions/write_message.py
+++ src/towns_actions/write_message.py
@@ -1,44 +1,62 @@
 import time
 import random
+from numpy import floor, ceil
 
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-from src.towns_profile_manager import TownsProfileManager
 from src.utils import send_keys
-from src.gpt_helper.openai_helper import generate_reply
 from data.lists import basic_colors
+from src.gpt_helper.openai_helper import generate_reply
+from src.towns_profile_manager import TownsProfileManager
 
 
-def write_message(towns_profile: TownsProfileManager, town_link):
+def write_n_messages(towns_profile: TownsProfileManager, town_link, n_messages=1, time_delay=15):
+    sent_messages = []
+
     # open town
     if town_link not in towns_profile.driver.current_url:
         towns_profile.driver.get(town_link)
 
-    # wait till page is loaded
+    # check if profile is a member of the town
+    check_element = WebDriverWait(towns_profile.driver, 20).until(EC.visibility_of_element_located(
+        (By.XPATH, "//*[contains(text(), 'Share Town Link')] | //*[contains(text(), 'Share Link')]")))
+
+    # check if profile is a member of the town
+    if "Share Town Link" in check_element.text:
+        print(f"You are not a member of this town. Link: {town_link}")
+        return False
+
+    # wait till Send_message element is loaded
     new_message_element = WebDriverWait(towns_profile.driver, 20).until(
         EC.visibility_of_element_located((By.XPATH, "//*[contains(text(), 'Send a message to ')]")))
 
-    # find messages
-    message_elements = towns_profile.driver.find_elements(By.XPATH, "//div[@data-testid='chat-message-container']")
+    for i in range(n_messages):
+        # find messages
+        message_elements = towns_profile.driver.find_elements(By.XPATH, "//div[@data-testid='chat-message-container']")
 
-    # get last 5 messages
-    last_messages = get_last_n_messages(message_elements, 5)
+        # get last 5 messages
+        last_messages = get_last_n_messages(message_elements, 5)
 
-    # generate next message
-    prompt = generate_prompt(last_messages)
-    next_message = generate_reply(prompt)
+        # generate next message
+        prompt = generate_prompt(last_messages, sent_messages)
+        next_message = generate_reply(prompt)
 
-    # find textbox and type next_message
-    new_message_element = new_message_element.find_element(By.XPATH, "..").find_element(By.XPATH,
-                                                                                        "//div[@role='textbox']")
-    send_keys(new_message_element, next_message)
-    time.sleep(1)
+        # find textbox and type next_message
+        new_message_element = new_message_element.find_element(By.XPATH, "..").find_element(By.XPATH,
+                                                                                            "//div[@role='textbox']")
+        send_keys(new_message_element, next_message)
+        time.sleep(1)
 
-    # find and click send button
-    send_message_element = towns_profile.driver.find_element(By.XPATH, "//button[@type='button' and @data-testid='submit']")
-    send_message_element.click()
+        # find and click send button
+        send_message_element = towns_profile.driver.find_element(By.XPATH, "//button[@type='button' and @data-testid='submit']")
+        send_message_element.click()
+
+        sent_messages.append(next_message)
+
+        # wait some time
+        time.sleep(random.randint(time_delay - floor(time_delay / 5), time_delay - ceil(time_delay / 5)))
 
 
 def get_last_n_messages(message_elements, n_messages=5):
@@ -62,7 +80,7 @@ def get_last_n_messages(message_elements, n_messages=5):
     return messages
 
 
-def generate_prompt(last_messages):
+def generate_prompt(last_messages, sent_messages):
     if len(last_messages) >= 0:
         if random.random() < 0.05:
             random_topic = random.sample(basic_colors, 1)[0]
@@ -70,7 +88,7 @@ def generate_prompt(last_messages):
         else:
             change_topic = ""
 
-        flow_of_messages = "\n".join(last_messages)
+        flow_of_messages = generate_message_flow(last_messages, sent_messages)
 
         content_message = f"""
 You are a casual Discord chat participant in your teens/early 20s. Your task is to continue the conversation naturally based on the previous messages.
@@ -110,3 +128,13 @@ Generate first message, please.
         """
 
     return content_message
+
+
+def generate_message_flow(last_messages, sent_messages):
+    message_flow = ""
+    for message in last_messages:
+        if message in sent_messages:
+            message_flow += f"Your message: {message}\n"
+        else:
+            message_flow += f"Smbd's message: {message}\n"
+    return message_flow[:-1]
